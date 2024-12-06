7b237287766b8f9f137b6e982de4b6cdf085a0cf
god123oriwke4ofjk <163017856+god123oriwke4ofjk@users.noreply.github.com>
2024-12-06 19:19:20 +0200

    Add files via upload

---
+++ botResponses.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from botFunc import handle_response
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@bot.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+
+    print(f"{username} said: '{user_message}' ({channel})")
+    response = await handle_response(message)
+    await bot.process_commands(message)
+    if response:
+        await message.channel.send(response)
+
+def run_discord_bot():
+    TOKEN = 'MTIyNzU3NDk0NjM0ODQwMDc3Mg.G2_Pth.-gVXjxQZVnbZVAFVXeW7Dkg7a41UOBhCjjHm3k'
+    bot.run(TOKEN)
+
+if __name__ == "__main__":
+    run_discord_bot()
